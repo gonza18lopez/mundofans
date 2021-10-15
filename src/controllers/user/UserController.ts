319f3676753432aa482b2a39fb6183efddccc340
Gonza Lopez <gonzalopez1532@hotmail.com>
2021-10-15 00:14:06 -0300

    [REMOVE] Badges adapter from user controller.

--- src/controllers/user/UserController.ts
+++ src/controllers/user/UserController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import UserService from '@services/UserService'
-import BadgeAdapter from "@services/adapters/BadgeAdapter";
 
 export default {
     /**
