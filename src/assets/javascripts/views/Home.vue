623fc81f4dcab386bff8c19e3db97b876c782f37
Gonza Lopez <gonzalopez1532@hotmail.com>
2021-10-19 16:30:20 -0300

    [ADD] Feature boxes

--- src/assets/javascripts/views/Home.vue
+++ src/assets/javascripts/views/Home.vue
@@ -1,7 +1,41 @@
 <template>
     <div class="row">
         <div class="col-12 mb-3 col-md-4">
-            <div class="card feature-box">
+            <div class="card feature-box bg-danger">
+                <div class="card-header">
+                    <div class="card-title">
+                        Title
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    xD
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 mb-3 col-md-4">
+            <div class="card feature-box bg-warning">
+                <div class="card-header">
+                    <div class="card-title">
+                        Title
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    xD
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 mb-3 col-md-4">
+            <div class="card feature-box bg-info">
+                <div class="card-header">
+                    <div class="card-title">
+                        Title
+                    </div>
+                </div>
+
                 <div class="card-body">
                     xD
                 </div>
